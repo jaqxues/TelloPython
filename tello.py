c77d7168d022fc21650c337c99e282fb4ffff07b
jaqxues <32979131+jaqxues@users.noreply.github.com>
2019-12-21 14:27:43 +0100

    Reformat + Correct Commands List

--- tello.py
+++ tello.py
@@ -1,6 +1,5 @@
-import queue
-import datetime
 import logging
+import queue
 import socket
 import threading
 import time
@@ -9,9 +8,24 @@ import time
 def _try_to_int(value):
     try:
         value = int(value)
-    except:
+    except ValueError:
         pass
     return value
+
+
+def validate_distance(distance):
+    distance = min(distance, 500)
+    return max(distance, 20)
+
+
+def validate_angle(degree):
+    degree = min(degree, 3600)
+    return max(degree, 1)
+
+
+def validate_speed(speed):
+    speed = min(speed, 100)
+    return max(speed, 10)
 
 
 class Drone:
@@ -24,10 +38,7 @@ class Drone:
         self.state_interval = state_interval
         self.command_timeout = command_timeout
 
-        self.abort_flag = False
-
         self.response_queue = queue.Queue(1)
-        self.state_last_update = None
         self.states = {}
 
         self.tello_address = (tello_ip, self.TELLO_COMMAND_PORT)
@@ -60,6 +71,8 @@ class Drone:
                     self.response_queue.put(data.decode(encoding='utf-8'))
             except socket.error:
                 logging.error('Ack Socket Failed')
+            except UnicodeDecodeError:
+                logging.error('Illegal Answer?')
 
     def _receive_state(self):
         while True:
@@ -69,20 +82,18 @@ class Drone:
                     data = data.decode(encoding='utf-8')
                     if ';' in data:
                         states = data.replace(';\r\n', '').split(';')
-                        self.states = {s[0]: s[1] for s in map(lambda item: item.split(';'), states)}
-
-                self.state_last_update = datetime.datetime.now()
+                        self.states = {s[0]: s[1] for s in map(lambda item: item.split(':'), states)}
 
                 time.sleep(self.state_interval)
             except socket.error:
                 logging.error('State Socket Failed')
+            except UnicodeDecodeError:
+                logging.error('Illegal Answer?')
 
     def send_command(self, command, command_timeout=None):
         if command_timeout is None:
             command_timeout = self.command_timeout
         print('>> Send Command:', command)
-
-        self.abort_flag = False
 
         self.socket.sendto(command.encode(encoding='utf-8'), self.tello_address)
 
@@ -107,15 +118,13 @@ class Drone:
         return self.send_command('takeoff', 10)  # Takeoff Command takes longer than other Commands
 
     def land(self):
-        return self.send_command('land')
+        return self.send_command('land', 10)  # Land Command can take longer than other Commands
 
     def emergency(self):
         return self.send_command('emergency')
 
     def _move(self, direction, distance):
-        distance = min(distance, 500)
-        distance = max(distance, 20)
-        return self.send_command(f'{direction} {distance}')
+        return self.send_command(f'{direction} {validate_distance(distance)}')
 
     def move_backward(self, distance):
         return self._move('back', distance)
@@ -132,36 +141,79 @@ class Drone:
     def move_up(self, distance):
         return self._move('up', distance)
 
+    def move_down(self, distance):
+        return self._move('down', distance)
+
+    def _turn(self, direction, degree):
+        return self.send_command(f'{direction} {validate_angle(degree)}')
+
+    def clockwise(self, degree):
+        return self._turn('cw', degree)
+
+    def counter_clockwise(self, degree):
+        return self._turn('ccw', degree)
+
+    def _flip(self, direction):
+        return self.send_command(f'flip {direction}')
+
+    def flip_left(self):
+        return self._flip('l')
+
+    def flip_right(self):
+        return self._flip('r')
+
+    def flip_forward(self):
+        return self._flip('f')
+
+    def flip_backward(self):
+        return self._flip('b')
+
     def go_location(self, x, y, z, speed):
-        return self.send_command(f'go {x} {y} {z} {speed}')
+        return self.send_command(f'go {validate_distance(x)} {validate_distance(y)}'
+                                 f'{validate_distance(z)} {speed}')
 
     def curve(self, x1, y1, z1, x2, y2, z2, speed):
-        return self.send_command(f'curve {x1} {y1} {z1} {x2} {y2} {z2} {speed}')
+        return self.send_command(f'curve {validate_distance(x1)} {validate_distance(y1)} {validate_distance(z1)} '
+                                 f'{validate_distance(x2)} {validate_distance(y2)} {validate_distance(z2)} {speed}')
 
-    def get_wifi(self):
+    def set_speed(self, speed):
+        return self.send_command(f'speed {validate_speed(speed)}')
+
+    def set_rc(self, a, b, c, d):
+        return self.send_command(f'rc {a} {b} {c} {d}')
+
+    def set_wifi_password(self, ssid, passwd):
+        return self.send_command(f'wifi {ssid} {passwd}')
+
+    def get_speed(self):  # Unit: cm/s
+        return _try_to_int(self.send_command('speed?'))
+
+    def get_battery(self):  # Unit: %
+        return _try_to_int(self.send_command('battery?'))
+
+    def get_flight_time(self):  # Unit: s
+        return _try_to_int(self.send_command('time?'))
+
+    def get_height(self):  # Unit: cm
+        return _try_to_int(self.send_command('height?'))
+
+    def get_temp(self):  # Unit: °C
+        return _try_to_int(self.send_command('temp?'))
+
+    def get_attitude(self):  # IMU Attitude Data
+        return self.send_command('attitude?')
+
+    def get_barometer(self):  # Unit: m
+        return self.send_command('baro?')
+
+    def get_acceleration(self):  # Unit: 0.001g
+        return self.send_command('acceleration?')
+
+    def get_tof_distance(self):  # Unit: cm
+        return self.send_command('tof?')
+
+    def get_wifi_snr(self):
         return self.send_command('wifi?')
-
-    def get_sdk(self):
-        return self.send_command('sdk?')
-
-    def get_serial_number(self):
-        return self.send_command('sn?')
-
-    def get_flight_time(self):
-        flight_time = self.send_command('time?')
-        return _try_to_int(flight_time)
-
-    def get_battery(self):
-        battery = self.send_command('battery?')
-        return _try_to_int(battery)
-
-    def get_speed(self):
-        speed = self.send_command('speed?')
-        try:
-            speed = round((float(speed) / 27.7778), 1)
-        except:
-            pass
-        return speed
 
     def get_last_states(self):
         return self.states
